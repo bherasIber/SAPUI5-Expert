b555fddb60fe6ab622fe4b93c16cf5aa4c61b0da
bherasIber <bheras@ibercacao.es>
2024-09-20 14:02:47 +0000

    Ejercicos nuevos

--- invoices/webapp/controller/App.controller.js
+++ invoices/webapp/controller/App.controller.js
@@ -1,14 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
+    "sap/ui/core/mvc/Controller"
 
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.m.MessageToast} MessageToast
      */
-    function (Controller, MessageToast) {
+    function (Controller) {
         //alert("UI5 is ready");
         "use strict";
 
@@ -16,13 +14,6 @@ sap.ui.define([
             onInit: function () {
 
             },
-            onShowHello: function () {
-                //alert("Hello World");
-                //read text from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMsg);
-            }
+            
         });
     });
