09386f7bcbbf7bdcdc9b8b73b4e3d22ef3b301bf
bherasIber <bheras@ibercacao.es>
2024-09-20 15:54:32 +0000

    Añadir mas funcionalidad

--- invoices/webapp/controller/HelloPanel.controller.js
+++ invoices/webapp/controller/HelloPanel.controller.js
@@ -1,14 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-
+    "sap/ui/core/Fragment"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.core.Fragment} Fragment
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         //alert("UI5 is ready");
         "use strict";
 
@@ -23,6 +24,11 @@ sap.ui.define([
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
                 var sMsg = oBundle.getText("helloMsg", [sRecipient]);
                 MessageToast.show(sMsg);
+            },
+            onOpenDialog: function () {
+
+                
             }
+            
         });
     });
