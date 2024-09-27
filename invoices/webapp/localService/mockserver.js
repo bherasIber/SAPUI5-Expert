84500c2faded356a5740bda25fc0d933b385da54
Borja de las Heras <bheras@ibercacao.es>
2024-09-27 15:43:46 +0000

    Primer commit

---
+++ invoices/webapp/localService/mockserver.js
@@ -0,0 +1,115 @@
+sap.ui.define([
+    "sap/ui/core/util/MockServer",
+    "sap/ui/model/json/JSONModel",
+    "sap/base/util/UriParameters",
+    "sap/base/Log"
+],
+    /**
+     * @param{ type of sap.ui.core.util.MockServer } MockServer
+     * @param{ type of sap.ui.model.json.JSONModel } MockServer
+     * @param{ type of sap.base.util.UriParameters } MockServer
+     * @param{ type of sap.base.Log } MockServer
+     */
+    function (MockServer, JSONModel, UriParameters, Log) {
+        "use strict";
+
+        var oMockServer,
+            _sAppPath = "logaligroup/invoices",
+            _sJsonFilesPath = -_sAppPath + "localService/mockdata";
+
+        var oMockServerInterface = {
+            /**
+             * Initializes the mock server asynchronously
+             * @protected
+             * @param {object} oOptionParameter
+             * @returns {Promise} a primise that is resolved when the mock server has been statick
+             */
+            init: function(oOptionParameter) {
+
+                var oOptions = oOptionParameter || {};
+
+                return new Promise (function(fnResolve, fnReject) {
+                    var sManifestUrl = sap.ui.require.toUrl(_sAppPath + "manifest.json"),
+                        oManifestModel = new JSONModel(sManifestUrl);
+                    
+                    oManifestModel.attachRequestCompeted(function() {
+                        var oUriParameters = new UriParameters(window.location.href);
+
+                        //parse manifest for local metadata URI
+                        var sJsonFilesUrl = sap.ui.require.toUrl(_sJsonFilesPath);
+                        var oMainDataSource = oManifestModel.getProperty("/sap.app/dataSources/mainService");
+                        var sMetadataUrl = sap.ui.require.toUrl(_sAppPath + oMainDataSource.setting.localUri);
+
+                        //ensure there is a trailing slash
+                        var sMockServerUrl = oMainDataSource.uri && new URIError(pMainDataSource.uri).absoluteTo(sap.ui.require.toUrl(_sAppPath)).toString();
+
+                        //Create a mock server instance or stop the existing on to reinitialize
+                        if(!oMockServer) {
+                            oMockServer = new MockServer({
+                                rootUri: sMockServerUrl
+                            })
+                        } else {
+                            oMockServer.stop();
+                        };
+
+                        //Configure mock server with the given options or a default delay of 0.5s
+                        MockServer.config({
+                            autoRespond: true,
+                            autoRespondAfter: ( oOptionParameter.delay || oUriParameters.get("serverDelay") || 500 ) 
+                        });
+
+                        //Simulate all requests using mock data
+                        oMockServer.simulate(sMetadataUrl, {
+                            sMockDataBaseUrl: sJsonFilesUrl,
+                            bGenerateMissingMockData: true
+                        });
+
+                        var aRequests = oMockServer.getRequests();
+
+                        //Compose an error response for each request
+                        var fnResponse = function(iErrCode, sMessage, aRequest) {
+                            aRequest.response = function (oXhr) {
+                                oXhr.response(iErrCode, {"Content-Type" : "text/plain;charset=utf-8"}, sMessage);
+                            };
+                        };
+
+                        //Simulate metadata errors
+                        if (oOptions.metadataError || oUriParameters.get("metadataError")) {
+                            aRequests.array.forEach(function (aEntry) {
+                                if (aEntry.path.toString().indexOf("$metadata") > -1){
+                                    fnResponse(500, "metadata Erro", aEntry);
+                                }
+                            });
+                        };
+
+                        //Simulate request error
+                        var sErrorParam = oOptions.errorType || oUriParameters.get("errorType");
+                        var iErrorCode = sErrorParam === "badRequest" ? 400 : 500;
+                        
+                        if (sErrorParam){
+                            aRequests.forEach(function (aEntry) {
+                                fnResponse(iErrorCode, sErrorParam, aEntry);
+                            });
+                        };
+
+                        //set request and start the server
+                        oMockServer.setRequests(aRequests);
+                        oMockServer.start();
+
+                        Log.info("Running the app with mock data");
+                        fnResolve();
+
+                    });
+
+                    oManifestModel.attachRequestFailed(function () {
+                        var sError = "Failed to load the application manifest";
+
+                        Log.error(sError);
+                        fnReject( new Error(sError));
+                    });
+                });
+            }
+        };
+
+        return oMockServerInterface;
+    });
---
+++ invoices/webapp/test/initMockServer.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "../localService/mockserver",
+    "sap/m/MessageBox"
+], 
+/**
+ * @param{ type of sap.m.Messagebox} Messagebox
+ */
+function(mockserver,Messagebox){
+
+    "use strict";
+
+    var aMockServers = [];
+
+    //Inicialize the mock server
+    aMockServers.push(mockserver.init());
+    
+    Promise.all(aMockServers).catch( function (oError){
+        Messagebox.error(oError.message);
+    }).finally( function(){
+        sap.ui.require(["module:sap/ui/core/ComponentSupport"]);
+    });
+
+});
