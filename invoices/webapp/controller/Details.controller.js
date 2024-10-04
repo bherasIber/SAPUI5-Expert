3338d66a816fd011deca229381a941a760c50470
Borja de las Heras <bheras@ibercacao.es>
2024-10-04 08:16:53 +0000

    Primera subida

---
+++ invoices/webapp/controller/Details.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     */
+    function (Controller) {
+        //alert("UI5 is ready");
+        "use strict";
+
+        return Controller.extend("logaligroup.invoices.controller.Details", {
+            onInit: function () {
+
+            }
+            
+        });
+    });
